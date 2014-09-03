6ea7daf5e36f236715db36683d9d0135509bba6e
Dragos STOICA <dragos_constantin_stoica@yahoo.com>
2014-09-03 09:00:00 +0300

    Appzip - v0.1
    Couchapp to publish couch application in zip format.

---
+++ push2couch.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#
+# Push to CouchDB all files in this directory
+# as attachetemts to _design/appzip in database
+# appzip
+
+
+#
+# Cleanup first
+# Delete database
+#
+
+curl -X DELETE http://172.16.1.79:5984/appzip
+curl -X PUT http://172.16.1.79:5984/appzip
+curl -X PUT http://172.16.1.79:5984/appzip/_design/appzip -d {}
+
+for file in *; do
+    rev="$(curl -sS http://172.16.1.79:5984/appzip/_design/appzip | sed -ne 's/^.*"_rev":"\([^"]*\)".*$/\1/p')"
+	#echo $rev 
+	
+	contenttype="Content-Type: application/octet-stream"
+	
+	case "$file" in
+	*.txt | *.map) 
+			contenttype="Content-Type: text/plain"
+	        ;;
+	*.js)
+			contenttype="Content-Type: application/x-javascript"
+			;;
+	*.html)
+			contenttype="Content-Type: text/html"
+			;;
+	*.css)
+			contenttype="Content-Type: text/css"
+			;;
+	*.jpg | *.jpeg)
+			contenttype="Content-Type: image/jpeg"
+			;;
+	*.gif)
+			contenttype="Content-Type: image/gif"
+			;;
+	*.png)
+			contenttype="Content-Type: image/png"
+			;;			
+	*)
+			contenttype="Content-Type: application/octet-stream"
+	        ;;
+	esac
+	
+	
+	curl -vX PUT http://172.16.1.79:5984/appzip/_design/appzip/$file?rev="$rev" --data-binary @"$file" -H "$contenttype"
+done
